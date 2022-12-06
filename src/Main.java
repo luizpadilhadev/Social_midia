b4effde988a72ff03d3fead659c157a9f5a87e03
luizpadilhadev <luizfelipepadilha86@gmail.com>
2022-12-06 19:56:15 -0300

    projeto finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
+import Entities.Comment;
+import Entities.Post;
 
-        System.out.println("Hello world!");
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Comment c1 = new Comment("Have a nice trip!");
+        Comment c2 = new Comment("Wow that's Awesome!");
+        Post p1 = new Post(sdf.parse(
+                "21/06/2018 13:05:44"),
+                "Traveling to new zealand",
+                "I'm going to visit this wonderful country!",
+                12);
+        p1.addComment(c1);
+        p1.addComment(c2);
+
+        System.out.println(p1);
+
     }
 }
